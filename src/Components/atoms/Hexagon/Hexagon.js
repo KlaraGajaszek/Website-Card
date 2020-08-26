a666a389954068f33383df2510dcab5efc4751be
KlaraGajaszek <klaragajaszek21@wp.pl>
2020-08-26 14:44:45 +0200

    small fixes

--- src/Components/atoms/Hexagon/Hexagon.js
+++ src/Components/atoms/Hexagon/Hexagon.js
@@ -3,9 +3,9 @@ import './HexagonStyles.css';
 
 const Hexagon = () => {
   return (
-    <div class="hexagon">
-      <div class="triangle t1"></div>
-      <div class="triangle t2"></div>
+    <div className="hexagon">
+      <div className="triangle t1"></div>
+      <div className="triangle t2"></div>
     </div>
   );
 };
